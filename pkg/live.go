df37a38af3f3f2aad2b052a65e3b5aa9da6a46bd
ltfred <ltfred@163.com>
2026-02-05 17:58:58 +0800

    feat: theme live model

--- pkg/live.go
+++ pkg/live.go
@@ -11,6 +11,9 @@ type liveMode struct {
 	theme     theme
 	maxWidth  int
 	maxHeight int
+
+	t               Theme
+	backgroundStyle lipgloss.Style
 }
 
 func (m liveMode) Init() tea.Cmd {
@@ -22,74 +25,67 @@ func (m liveMode) Update(message tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m liveMode) View() string {
-	theme := decodeTheme(m.theme.data)
-	backgroundStyle := lipgloss.NewStyle().Background(lipgloss.Color(theme.Colors.Primary.Background))
-
-	themeNameStyle := backgroundStyle.Bold(true).Padding(1, 1).Italic(true).
-		Width(m.maxWidth).Foreground(lipgloss.Color(theme.Colors.Primary.Foreground)).Align(lipgloss.Center)
-
-	colorBlockStyle := backgroundStyle.Padding(1, 1).Width(m.maxWidth)
-
-	// 创建彩色方块
-	colorNames := []string{"Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White"}
-	colors := make([]colorBlock, 0, len(colorNames))
-	for _, name := range colorNames {
-		normalColor := reflect.ValueOf(theme.Colors.Normal).FieldByName(name).String()
-		brightColor := reflect.ValueOf(theme.Colors.Bright).FieldByName(name).String()
-		colors = append(colors, colorBlock{
-			normalColor: normalColor,
-			brightColor: brightColor,
-			name:        name,
-		})
-	}
-	colorBlock := renderColorBlock(theme.Colors.Primary.Background, theme.Colors.Primary.Foreground, colors)
-
-	alacritty := backgroundStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: theme.Colors.Bright.Cyan,
-		Dark:  theme.Colors.Normal.Cyan,
-	}).Render("alacritty")
-
-	branch := backgroundStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: theme.Colors.Bright.Blue,
-		Dark:  theme.Colors.Normal.Blue,
-	}).Render("\uF418 main")
-
-	stash := backgroundStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: theme.Colors.Bright.Red,
-		Dark:  theme.Colors.Normal.Red,
-	}).Render("[+]")
-
-	program := backgroundStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: theme.Colors.Bright.Yellow,
-		Dark:  theme.Colors.Normal.Yellow,
-	}).Render("\uE626 v1.26")
-
-	on := backgroundStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: theme.Colors.Bright.Yellow,
-		Dark:  theme.Colors.Normal.Yellow,
-	}).Render("on")
-	via := backgroundStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: theme.Colors.Bright.Yellow,
-		Dark:  theme.Colors.Normal.Yellow,
-	}).Render("via")
-
-	blank := backgroundStyle.Render(" ")
-
-	k8s := backgroundStyle.Foreground(lipgloss.AdaptiveColor{
-		Light: theme.Colors.Bright.Blue,
-		Dark:  theme.Colors.Normal.Blue,
-	}).Render("\uE81D (alacritty-env)")
-
-	prompt := backgroundStyle.Padding(1, 1).Width(m.maxWidth).Render(alacritty + blank + on + blank + branch + blank + stash + blank + program + blank + via + blank + k8s)
+	m.t = decodeTheme(m.theme.data)
+	m.backgroundStyle = lipgloss.NewStyle().Background(lipgloss.Color(m.t.Colors.Primary.Background))
 
 	view := lipgloss.JoinVertical(
 		lipgloss.Top,
-		themeNameStyle.Render(m.theme.name),
-		colorBlockStyle.Render(colorBlock),
-		prompt,
+		m.renderColorName(),
+		m.renderColorBlock(),
+		m.renderPrompt(),
+		//backgroundStyle.Width(m.maxWidth).Render("\uF51F"),
 	)
 
-	return backgroundStyle.Height(m.maxHeight).Width(m.maxWidth).Render(view)
+	return m.backgroundStyle.Height(m.maxHeight).Width(m.maxWidth).Render(view)
+}
+
+func (m liveMode) renderColorName() string {
+	backgroundStyle := lipgloss.NewStyle().Background(lipgloss.Color(m.t.Colors.Primary.Background))
+	return backgroundStyle.Bold(true).Italic(true).
+		Width(m.maxWidth).Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Primary.BrightForeground,
+		Dark:  m.t.Colors.Primary.Foreground,
+	}).Align(lipgloss.Center).Render(m.theme.name)
+}
+
+func (m liveMode) renderPrompt() string {
+	alacritty := m.backgroundStyle.Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Bright.Cyan,
+		Dark:  m.t.Colors.Normal.Cyan,
+	}).Render("alacritty")
+
+	branch := m.backgroundStyle.Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Bright.Blue,
+		Dark:  m.t.Colors.Normal.Blue,
+	}).Render("\uF418 main")
+
+	stash := m.backgroundStyle.Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Bright.Red,
+		Dark:  m.t.Colors.Normal.Red,
+	}).Render("[+]")
+
+	program := m.backgroundStyle.Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Bright.Yellow,
+		Dark:  m.t.Colors.Normal.Yellow,
+	}).Render("\U000F07D3 v1.26")
+
+	on := m.backgroundStyle.Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Bright.Yellow,
+		Dark:  m.t.Colors.Normal.Yellow,
+	}).Render("on")
+	via := m.backgroundStyle.Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Bright.Yellow,
+		Dark:  m.t.Colors.Normal.Yellow,
+	}).Render("via")
+
+	blank := m.backgroundStyle.Render(" ")
+
+	k8s := m.backgroundStyle.Foreground(lipgloss.AdaptiveColor{
+		Light: m.t.Colors.Bright.Blue,
+		Dark:  m.t.Colors.Normal.Blue,
+	}).Render("\uE81D (alacritty-env)")
+
+	return m.backgroundStyle.Width(m.maxWidth).Render(alacritty + blank + on + blank + branch + blank + stash + blank + program + blank + via + blank + k8s)
 }
 
 type colorBlock struct {
@@ -98,38 +94,51 @@ type colorBlock struct {
 	name        string
 }
 
-func renderColorBlock(background, foreground string, colors []colorBlock) string {
+func (m liveMode) renderColorBlock() string {
+	// 创建彩色方块
+	colorNames := []string{"Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White"}
+	colors := make([]colorBlock, 0, len(colorNames))
+	for _, name := range colorNames {
+		normalColor := reflect.ValueOf(m.t.Colors.Normal).FieldByName(name).String()
+		brightColor := reflect.ValueOf(m.t.Colors.Bright).FieldByName(name).String()
+		colors = append(colors, colorBlock{
+			normalColor: normalColor,
+			brightColor: brightColor,
+			name:        name,
+		})
+	}
+
 	var allElements []string
 	allElements = append(allElements,
 		lipgloss.NewStyle().Render(
 			lipgloss.JoinVertical(
 				lipgloss.Left,
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Foreground(lipgloss.Color(foreground)).Width(7).Render(""),
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Width(7).Render(""),
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Width(7).Height(3).Foreground(lipgloss.Color(foreground)).Render("Normal"),
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Width(7).Render(""),
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Width(7).Height(3).Foreground(lipgloss.Color(foreground)).Render("Bright"),
+				m.backgroundStyle.Foreground(lipgloss.Color(m.t.Colors.Primary.Foreground)).Width(7).Render(""),
+				m.backgroundStyle.Width(7).Render(""),
+				m.backgroundStyle.Width(7).Height(3).Foreground(lipgloss.Color(m.t.Colors.Primary.Foreground)).Render("Normal"),
+				m.backgroundStyle.Width(7).Render(""),
+				m.backgroundStyle.Width(7).Height(3).Foreground(lipgloss.Color(m.t.Colors.Primary.Foreground)).Render("Bright"),
 			),
 		),
-		lipgloss.NewStyle().Width(2).Height(9).Background(lipgloss.Color(background)).Render(""),
+		lipgloss.NewStyle().Width(2).Height(9).Background(lipgloss.Color(m.t.Colors.Primary.Background)).Render(""),
 	)
 
 	for i, color := range colors {
 		s := lipgloss.NewStyle().Render(
 			lipgloss.JoinVertical(
 				lipgloss.Left,
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Foreground(lipgloss.Color(foreground)).Width(7).Align(lipgloss.Center).Render(color.name),
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Width(7).Render(""),
+				m.backgroundStyle.Foreground(lipgloss.Color(m.t.Colors.Primary.Foreground)).Width(7).Align(lipgloss.Center).Render(color.name),
+				m.backgroundStyle.Width(7).Render(""),
 				lipgloss.NewStyle().Background(lipgloss.Color(color.normalColor)).Width(7).Height(3).Render(""),
-				lipgloss.NewStyle().Background(lipgloss.Color(background)).Width(7).Render(""),
+				m.backgroundStyle.Width(7).Render(""),
 				lipgloss.NewStyle().Background(lipgloss.Color(color.brightColor)).Width(7).Height(3).Render(""),
 			),
 		)
 
 		if i > 0 {
-			allElements = append(allElements, lipgloss.NewStyle().Width(2).Height(9).Background(lipgloss.Color(background)).Render(""))
+			allElements = append(allElements, m.backgroundStyle.Width(2).Height(9).Render(""))
 		}
 		allElements = append(allElements, s)
 	}
-	return lipgloss.NewStyle().Background(lipgloss.Color(background)).Render(lipgloss.JoinHorizontal(lipgloss.Left, allElements...))
+	return m.backgroundStyle.Width(m.maxWidth).Render(lipgloss.JoinHorizontal(lipgloss.Left, allElements...))
 }
